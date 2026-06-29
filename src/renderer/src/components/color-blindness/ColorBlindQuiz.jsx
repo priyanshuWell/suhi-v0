09cb769f5a9f1d154139765faa2001d8bda37d02
suhi <shubhamtripathi@wellwiz.co.in>
2026-06-29 13:51:30 +0530

    config changes

--- src/renderer/src/components/color-blindness/ColorBlindQuiz.jsx
+++ src/renderer/src/components/color-blindness/ColorBlindQuiz.jsx
@@ -201,7 +201,7 @@ export const ColorBlindQuiz = () => {
                 );
             } catch (err) {
                 console.error("colorBlindessSubmit error:", err);
-                navigate("/voice");
+                navigate("/bia/result");
                 return;
             }
 
