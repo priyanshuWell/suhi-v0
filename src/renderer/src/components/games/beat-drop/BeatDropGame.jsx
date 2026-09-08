34e4e8f2a2de23e975d221e87769edc709f5b792
suhi <shubhamtripathi@wellwiz.co.in>
2026-09-08 20:17:05 +0530

    add dumy

--- src/renderer/src/components/games/beat-drop/BeatDropGame.jsx
+++ src/renderer/src/components/games/beat-drop/BeatDropGame.jsx
@@ -75,7 +75,8 @@ export default function BeatDropGame() {
     }, [])
 
     const handleNext = () => {
-        const nextStage = screening?.nextStage
+        // const nextStage = screening?.nextStage
+        const nextStage = "/bia/result"
         const route = getNextRoute(nextStage, "/bia/result")
         navigate(route)
     }
